dbc1b2d595f62bb0dbf6622ecde78d5cc66ebe01
Peter Donald <peter@realityforge.org>
2014-01-15 18:35:22 +1100

    Add database setup scripts and local development templates

---
+++ config/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+R=`(asadmin list-libraries | grep -q postgresql-9.1-901.jdbc4.jar) && echo yes`
+if [ "$R" != 'yes' ]; then
+  asadmin add-library ~/.m2/repository/postgresql/postgresql/9.1-901.jdbc4/postgresql-9.1-901.jdbc4.jar
+fi
+
+asadmin delete-jdbc-resource jdbc/Tyrell
+asadmin delete-jdbc-connection-pool TyrellPool
+
+asadmin create-jdbc-connection-pool\
+  --datasourceclassname org.postgresql.ds.PGSimpleDataSource\
+  --restype javax.sql.DataSource\
+  --isconnectvalidatereq=true\
+  --validationmethod auto-commit\
+  --ping true\
+  --description "Tyrell Connection Pool"\
+  --property "ServerName=127.0.0.1:User=${USER}:Password=letmein:PortNumber=5432:DatabaseName=${USER}_TYRELL_DEV" TyrellPool
+asadmin create-jdbc-resource --connectionpoolid TyrellPool jdbc/Tyrell
+
+asadmin set domain.resources.jdbc-connection-pool.TyrellPool.property.JDBC30DataSource=true
