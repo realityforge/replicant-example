0af57ed4345cfbbf85dfe55513c0a2607779f470
Peter Donald <peter@realityforge.org>
2014-01-15 19:13:37 +1100

    Rework callback so that it emits logs to the console. Less annoying than popups

--- src/main/java/org/realityforge/replicant/example/client/GlobalAsyncCallback.java
+++ src/main/java/org/realityforge/replicant/example/client/GlobalAsyncCallback.java
@@ -1,7 +1,8 @@
 package org.realityforge.replicant.example.client;
 
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Callback invoked on completion of every job
@@ -9,14 +10,17 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 public class GlobalAsyncCallback
   implements AsyncCallback
 {
+  private static final Logger LOG = Logger.getLogger( GlobalAsyncCallback.class.getName() );
+
   @Override
   public void onFailure( final Throwable caught )
   {
-    Window.alert( "Failed!" );
+    LOG.log( Level.WARNING, "A request failed", caught );
   }
 
   @Override
   public void onSuccess( final Object result )
   {
+    LOG.log( Level.INFO, "A request succeeded: " + result );
   }
 }
