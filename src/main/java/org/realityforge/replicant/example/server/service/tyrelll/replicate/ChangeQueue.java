67c3e91838fbbbd77d915242c614eb50461aa08f
Peter Donald <peter@realityforge.org>
2014-01-16 14:31:48 +1100

    Import some more code to work towards replication

---
+++ src/main/java/org/realityforge/replicant/example/server/service/tyrelll/replicate/ChangeQueue.java
@@ -0,0 +1,25 @@
+package org.realityforge.replicant.example.server.service.tyrelll.replicate;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import javax.annotation.Nonnull;
+import javax.ejb.Local;
+import org.realityforge.replicant.server.EntityMessage;
+
+@Local
+public interface ChangeQueue
+{
+  /**
+   * Queue the specified messages to be saved as a change set.
+   *
+   * @param messages the messages.
+   */
+  void saveEntityMessages( @Nonnull Collection<EntityMessage> messages );
+
+  /**
+   * Route the global change set to the individual subscribers.
+   *
+   * @param changes the changes
+   */
+  void routeMessages( @Nonnull final LinkedList<Change> changes );
+}
