954623861686c39cdf82aaf1fd10eb606c3b82a5
Peter Donald <peter@realityforge.org>
2014-01-16 20:59:18 +1100

    Define a useful session for application

---
+++ src/main/java/org/realityforge/replicant/example/server/service/tyrell/TyrellSessionInfo.java
@@ -0,0 +1,84 @@
+package org.realityforge.replicant.example.server.service.tyrell;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.realityforge.replicant.example.server.service.tyrell.replicate.Change;
+import org.realityforge.replicant.example.server.service.tyrell.replicate.Packet;
+import org.realityforge.replicant.example.server.service.tyrell.replicate.ReplicantClient;
+import org.realityforge.replicant.server.EntityMessage;
+import org.realityforge.ssf.SessionInfo;
+import org.realityforge.ssf.SimpleSessionInfo;
+
+public class TyrellSessionInfo
+  extends SimpleSessionInfo
+  implements ReplicantClient
+{
+  private final LinkedList<Change> _changes = new LinkedList<>();
+  private final HashSet<Integer> _buildingsOfInterest = new HashSet<>();
+
+  // sequence of last packet delivered
+  private int _lastSequence;
+  private final LinkedList<Packet> _packets = new LinkedList<Packet>();
+
+
+  public TyrellSessionInfo( @Nonnull final String sessionID,
+                            @Nonnull final String username )
+  {
+    super( sessionID, username );
+  }
+
+  public void registerInterest( final int id )
+  {
+    _buildingsOfInterest.add( id );
+  }
+
+  public void deregisterInterest( final int id )
+  {
+    _buildingsOfInterest.remove( id );
+  }
+
+  public boolean isBuildingInteresting( final int id )
+  {
+    return _buildingsOfInterest.contains( id );
+  }
+
+  public final LinkedList<Change> getChanges()
+  {
+    return _changes;
+  }
+
+  @Override
+  public void addPacket( final List<EntityMessage> changeSet )
+  {
+    _packets.add( new Packet( newPacketSequence(), changeSet ) );
+  }
+
+  @Nullable
+  public synchronized Packet poll( final int lastSequenceAcked )
+  {
+    updateAccessTime();
+    final Iterator<Packet> iterator = _packets.iterator();
+    while ( iterator.hasNext() )
+    {
+      final Packet packet = iterator.next();
+      if ( packet.getSequence() <= lastSequenceAcked )
+      {
+        iterator.remove();
+      }
+      else
+      {
+        return packet;
+      }
+    }
+    return null;
+  }
+
+  private int newPacketSequence()
+  {
+    return _lastSequence++;
+  }
+}
---
+++ src/main/java/org/realityforge/replicant/example/server/service/tyrell/replicate/Packet.java
@@ -0,0 +1,35 @@
+package org.realityforge.replicant.example.server.service.tyrell.replicate;
+
+import java.util.List;
+import javax.annotation.Nonnull;
+import org.realityforge.replicant.server.EntityMessage;
+import org.realityforge.replicant.server.json.JsonEncoder;
+
+public final class Packet
+{
+  private final int _sequence;
+  @Nonnull
+  private final List<EntityMessage> _changes;
+
+  public Packet( final int sequence, @Nonnull final List<EntityMessage> changes )
+  {
+    _sequence = sequence;
+    _changes = changes;
+  }
+
+  public int getSequence()
+  {
+    return _sequence;
+  }
+
+  @Nonnull
+  public List<EntityMessage> getChanges()
+  {
+    return _changes;
+  }
+
+  public String toJson()
+  {
+    return JsonEncoder.encodeChangeSetFromEntityMessages( getSequence(), _changes );
+  }
+}
