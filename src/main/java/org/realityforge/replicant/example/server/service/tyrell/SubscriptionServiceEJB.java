ecb46abeac0c149cb0f216950ced6585ba97b518
Peter Donald <peter@realityforge.org>
2014-01-16 21:02:53 +1100

    Merge the SessionManager and the SubscriptionServiceEJB

--- src/main/java/org/realityforge/replicant/example/server/service/tyrell/SubscriptionServiceEJB.java
+++ src/main/java/org/realityforge/replicant/example/server/service/tyrell/SubscriptionServiceEJB.java
@@ -1,46 +1,135 @@
 package org.realityforge.replicant.example.server.service.tyrell;
 
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.annotation.PostConstruct;
+import javax.ejb.Local;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import org.realityforge.replicant.example.server.entity.tyrell.Building;
-import org.realityforge.replicant.example.server.service.tyrell.replicate.Change;
-import org.realityforge.replicant.example.server.service.tyrell.replicate.ChangeQueue;
+import org.realityforge.replicant.example.server.service.tyrell.replicate.EntityMessageAccumulator;
+import org.realityforge.replicant.example.server.service.tyrell.replicate.EntityRouter;
+import org.realityforge.replicant.example.server.service.tyrell.replicate.Packet;
 import org.realityforge.replicant.server.EntityMessage;
+import org.realityforge.replicant.server.EntityMessageEndpoint;
+import org.realityforge.replicant.server.EntityMessageGenerator;
+import org.realityforge.replicant.server.json.JsonEncoder;
+import org.realityforge.ssf.InMemorySessionManager;
+import org.realityforge.ssf.SessionManager;
 
 @Stateless
+@Local( { EntityMessageEndpoint.class, SubscriptionService.class, SessionManager.class } )
 public class SubscriptionServiceEJB
-  implements SubscriptionService, ChangeQueue
+  extends InMemorySessionManager<TyrellSessionInfo>
+  implements SubscriptionService, EntityMessageEndpoint
 {
-  @Nullable
-  @Override
-  public String poll( final int lastKnownChangeSetID, @Nonnull final String clientID )
+  private static final Logger LOG = Logger.getLogger( SubscriptionServiceEJB.class.getName() );
+
+  @Inject
+  private EntityMessageGenerator _messageGenerator;
+
+  private Encoder _encoder;
+
+  @PostConstruct
+  public void postConstruct()
   {
-    return null;
+    //_encoder = new Encoder( new TyrellEntityMessageGenerator( _router ) );
+    _encoder = new Encoder( _messageGenerator );
+  }
+
+  @Override
+  @Nullable
+  public String poll( @Nonnull final String clientID, final int lastSequenceAcked )
+    throws BadSessionException
+  {
+    final TyrellSessionInfo session = ensureSession( clientID );
+    final Packet packet = session.poll( lastSequenceAcked );
+    if ( null != packet )
+    {
+      return packet.toJson();
+    }
+    else
+    {
+      return null;
+    }
   }
 
   @Override
   @Nonnull
-  public String subscribeToBuilding( @Nonnull final String designator, @Nonnull final Building building )
+  public String subscribeToBuilding( @Nonnull final String clientID, @Nonnull final Building building )
+    throws BadSessionException
   {
-    return "";
+    final TyrellSessionInfo session = ensureSession( clientID );
+    session.registerInterest( building.getID() );
+    final LinkedList<EntityMessage> messages = new LinkedList<>();
+    _encoder.encodeBuilding( messages, building );
+    return JsonEncoder.encodeChangeSetFromEntityMessages( 0, messages );
   }
 
   @Override
   public void unsubscribeFromBuilding( @Nonnull final String clientID, @Nonnull final Building building )
+    throws BadSessionException
   {
+    ensureSession( clientID ).deregisterInterest( building.getID() );
   }
 
   @Override
   public void saveEntityMessages( @Nonnull final Collection<EntityMessage> messages )
   {
+    if ( LOG.isLoggable( Level.FINER ) )
+    {
+      LOG.fine( "EntityMessage messages generated during service execution: " + messages );
+    }
 
+    //TODO: Rewrite this so that we add clients to indexes rather than searching through everyone for each change!
+    final Map<String, TyrellSessionInfo> sessions = getSessions();
+    EntityMessageAccumulator<TyrellSessionInfo> accumulator = new EntityMessageAccumulator<>();
+    synchronized ( sessions )
+    {
+      for ( final EntityMessage message : messages )
+      {
+        final Map<String, Serializable> routingKeys = message.getRoutingKeys();
+
+        final Integer buildingID = (Integer) routingKeys.get( EntityRouter.BUILDING_KEY );
+        if ( null != buildingID )
+        {
+          for ( final TyrellSessionInfo sessionInfo : sessions.values() )
+          {
+            if ( sessionInfo.isBuildingInteresting( buildingID ) )
+            {
+              accumulator.addEntityMessage( sessionInfo, message );
+            }
+          }
+        }
+      }
+    }
+
+    accumulator.complete();
   }
 
+  @Nonnull
   @Override
-  public void routeMessages( @Nonnull final LinkedList<Change> changes )
+  protected TyrellSessionInfo newSessionInfo( @Nonnull final String username )
   {
+    return new TyrellSessionInfo( UUID.randomUUID().toString(), username );
+  }
+
+  @Nonnull
+  private TyrellSessionInfo ensureSession( @Nonnull final String clientID )
+    throws BadSessionException
+  {
+    final TyrellSessionInfo session = getSession( clientID );
+    if ( null == session )
+    {
+      throw new BadSessionException();
+    }
+    return session;
   }
 }
