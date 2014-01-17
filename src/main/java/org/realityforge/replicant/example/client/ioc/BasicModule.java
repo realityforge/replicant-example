72c8613d4d6fae92c15b1c46990751e16ff7d88b
Peter Donald <peter@realityforge.org>
2014-01-17 16:12:01 +1100

    Use the built in EntityChangeBroker

--- src/main/java/org/realityforge/replicant/example/client/ioc/BasicModule.java
+++ src/main/java/org/realityforge/replicant/example/client/ioc/BasicModule.java
@@ -6,12 +6,12 @@ import com.google.inject.name.Names;
 import com.google.web.bindery.event.shared.EventBus;
 import com.google.web.bindery.event.shared.SimpleEventBus;
 import org.realityforge.replicant.client.EntityChangeBroker;
+import org.realityforge.replicant.client.EntityChangeBrokerImpl;
 import org.realityforge.replicant.client.EntityRepository;
 import org.realityforge.replicant.client.EntityRepositoryImpl;
 import org.realityforge.replicant.example.client.GlobalAsyncCallback;
 import org.realityforge.replicant.example.client.services.DataLoaderService;
 import org.realityforge.replicant.example.client.services.TyrellDataLoaderService;
-import org.realityforge.replicant.example.client.services.TyrellEntityChangeBroker;
 
 public class BasicModule
   extends AbstractGinModule
@@ -21,7 +21,7 @@ public class BasicModule
   {
     bindNamedService( "GLOBAL", AsyncCallback.class, GlobalAsyncCallback.class );
     bind( EntityRepository.class ).to( EntityRepositoryImpl.class ).asEagerSingleton();
-    bind( EntityChangeBroker.class ).to( TyrellEntityChangeBroker.class ).asEagerSingleton();
+    bind( EntityChangeBroker.class ).to( EntityChangeBrokerImpl.class ).asEagerSingleton();
     bind( DataLoaderService.class ).to( TyrellDataLoaderService.class ).asEagerSingleton();
     bind( EventBus.class ).to( SimpleEventBus.class ).asEagerSingleton();
   }
