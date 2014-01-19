6edc2946f2b8169ef23d89831660e81fa8bf8749
Peter Donald <peter@realityforge.org>
2014-01-20 09:07:24 +1100

    Add a basic UI to interact with model

--- src/main/java/org/realityforge/replicant/example/client/ui/SimpleUI.java
+++ src/main/java/org/realityforge/replicant/example/client/ui/SimpleUI.java
@@ -8,11 +8,14 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Tree;
 import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.web.bindery.event.shared.EventBus;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 import javax.inject.Inject;
 import org.realityforge.replicant.client.EntityChangeBroker;
@@ -37,7 +40,12 @@ public class SimpleUI
   private final Button _update;
   private final Button _connect;
   private final Button _disconnect;
-  private final Button _createBuilding;
+  private final Map<Object, TreeItem> _viewMap = new HashMap<>();
+  private final Label _selected;
+  private final TextBox _input;
+  private final DataLoaderService _dataLoaderService;
+  private final GwtRpcBuildingService _buildingService;
+  private final EntityChangeBroker _broker;
   private Building _selectedBuilding;
   private Room _selectedRoom;
 
@@ -49,18 +57,19 @@ public class SimpleUI
   {
     super();
 
-    final TextBox input = new TextBox();
-    input.setValue( "Greetings!" );
+    _broker = broker;
+    _dataLoaderService = dataLoaderService;
+    _buildingService = buildingService;
+    _input = new TextBox();
+    _input.setValue( "MyBuilding" );
+    _input.setEnabled( false );
 
     _connect = new Button( "Connect", new ClickHandler()
     {
       @Override
       public void onClick( final ClickEvent event )
       {
-        _connect.setEnabled( false );
-        _createBuilding.setEnabled( true );
-        _disconnect.setEnabled( true );
-        dataLoaderService.connect();
+        doConnect();
       }
     } );
     _disconnect = new Button( "Disconnect", new ClickHandler()
@@ -68,68 +77,28 @@ public class SimpleUI
       @Override
       public void onClick( ClickEvent event )
       {
-        dataLoaderService.disconnect();
-        _connect.setEnabled( true );
-        _createBuilding.setEnabled( false );
-        _disconnect.setEnabled( false );
+        doDisconnect();
       }
     } );
     _disconnect.setEnabled( false );
 
-    _createBuilding = new Button( "Create and Subscribe to building", new ClickHandler()
-    {
-      @Override
-      public void onClick( ClickEvent event )
-      {
-        buildingService.createBuilding( input.getValue(), new TyrellGwtRpcAsyncCallback<Integer>()
-        {
-          @Override
-          public void onSuccess( final Integer result )
-          {
-            dataLoaderService.subscribeToBuilding( result );
-          }
-        } );
-
-      }
-    } );
-    _createBuilding.setEnabled( false );
-
     final VerticalPanel panel = new VerticalPanel();
-    {
-      final FlowPanel controls = new FlowPanel();
-      controls.add( _connect );
-      controls.add( _disconnect );
-      panel.add( controls );
-    }
+    final FlowPanel controls = new FlowPanel();
+    controls.add( _connect );
+    controls.add( _disconnect );
+    panel.add( controls );
 
-    {
-      final FlowPanel controls = new FlowPanel();
-      controls.add( input );
-      controls.add( _createBuilding );
-      panel.add( controls );
-    }
     _tree = new Tree();
+    _tree.setHeight( "200px" );
+    _tree.setWidth( "100%" );
+    _tree.setScrollOnSelectEnabled( true );
 
     _create = new Button( "Create", new ClickHandler()
     {
       @Override
-      public void onClick( ClickEvent event )
+      public void onClick( final ClickEvent event )
       {
-        if ( null == _selectedBuilding )
-        {
-          buildingService.createBuilding( input.getValue(), new TyrellGwtRpcAsyncCallback<Integer>()
-          {
-            @Override
-            public void onSuccess( final Integer result )
-            {
-              dataLoaderService.subscribeToBuilding( result );
-            }
-          } );
-        }
-        else
-        {
-          buildingService.createRoom( _selectedBuilding.getID(), 1, 1, input.getName(), true );
-        }
+        onCreate();
       }
     } );
     _create.setEnabled( false );
@@ -137,25 +106,21 @@ public class SimpleUI
     _update = new Button( "Update", new ClickHandler()
     {
       @Override
-      public void onClick( ClickEvent event )
+      public void onClick( final ClickEvent event )
       {
-        if ( null != _selectedBuilding )
-        {
-          buildingService.setBuildingName( _selectedBuilding.getID(), input.getValue() );
-        }
-        else
-        {
-          buildingService.setRoomName( _selectedRoom.getID(), input.getName() );
-        }
+        onUpdate();
       }
     } );
     _update.setEnabled( false );
 
     final HorizontalPanel control = new HorizontalPanel();
+    control.add( _input );
     control.add( _create );
     control.add( _update );
 
     panel.add( control );
+    _selected = new Label();
+    panel.add( _selected );
     panel.add( _tree );
 
     _tree.addSelectionHandler( new SelectionHandler<TreeItem>()
@@ -163,29 +128,10 @@ public class SimpleUI
       @Override
       public void onSelection( final SelectionEvent<TreeItem> event )
       {
-        final Object userObject = event.getSelectedItem().getUserObject();
-        _selectedBuilding = null;
-        _selectedRoom = null;
-        if ( userObject instanceof Building )
-        {
-          _selectedBuilding = (Building) userObject;
-          _create.setText( "Create Room" );
-          _create.setEnabled( true );
-          _update.setEnabled( true );
-        }
-        else if ( userObject instanceof Room )
-        {
-          _selectedRoom = (Room) userObject;
-          _create.setEnabled( false );
-          _update.setEnabled( true );
-        }
-        else
-        {
-          _update.setEnabled( false );
-        }
+        onSelect( event.getSelectedItem().getUserObject() );
       }
     } );
-    broker.addChangeListener( this );
+    _broker.addChangeListener( this );
     eventBus.addHandler( BuildingDataLoadedEvent.TYPE, new BuildingDataLoadedEventHandler()
     {
       @Override
@@ -194,33 +140,193 @@ public class SimpleUI
         final TreeItem treeItem = _tree.addTextItem( event.getBuilding().getName() );
         final Building building = event.getBuilding();
         treeItem.setUserObject( building );
-
+        treeItem.setState( true );
+        _viewMap.put( building, treeItem );
       }
     } );
     initWidget( panel );
+  }
+
+  private void onSelect( final Object userObject )
+  {
+    _selectedBuilding = null;
+    _selectedRoom = null;
+    if ( userObject instanceof Building )
+    {
+      _selectedBuilding = (Building) userObject;
+      _selected.setText( "Selected Building: " + _selectedBuilding.getName() );
+      _create.setText( "Create Room" );
+      _create.setEnabled( true );
+      _update.setEnabled( true );
+    }
+    else if ( userObject instanceof Room )
+    {
+      _selectedRoom = (Room) userObject;
+      _selected.setText( "Selected Room: " + _selectedRoom.getName() );
+      _create.setEnabled( false );
+      _update.setEnabled( true );
+    }
+    else
+    {
+      _selected.setText( "" );
+      _create.setEnabled( false );
+      _update.setEnabled( false );
+    }
+  }
+
+  private void onUpdate()
+  {
+    if ( null != _selectedBuilding )
+    {
+      _buildingService.setBuildingName( _selectedBuilding.getID(), _input.getValue() );
+    }
+    else
+    {
+      _buildingService.setRoomName( _selectedRoom.getID(), _input.getValue() );
+    }
+    _input.setValue( "" );
+  }
+
+  private void onCreate()
+  {
+    if ( null == _selectedBuilding )
+    {
+      _buildingService.createBuilding( _input.getValue(), new TyrellGwtRpcAsyncCallback<Integer>()
+      {
+        @Override
+        public void onSuccess( final Integer result )
+        {
+          _dataLoaderService.subscribeToBuilding( result );
+        }
+      } );
+    }
+    else
+    {
+      _buildingService.createRoom( _selectedBuilding.getID(), 1, 1, _input.getValue(), true );
+    }
+    _input.setValue( "" );
+  }
+
+  private void doDisconnect()
+  {
+    _dataLoaderService.disconnect();
+    _connect.setEnabled( true );
+    _create.setEnabled( false );
+    _update.setEnabled( false );
+    _input.setEnabled( false );
+    _tree.clear();
+    _disconnect.setEnabled( false );
+  }
+
+  private void doConnect()
+  {
+    _connect.setEnabled( false );
+    _create.setEnabled( true );
+    _input.setEnabled( true );
+    _disconnect.setEnabled( true );
+    _tree.clear();
+    _dataLoaderService.connect();
   }
 
   @Override
   public void entityRemoved( final EntityChangeEvent event )
   {
     LOG.info( "entityRemoved(" + event + ")" );
+    final Object entity = event.getObject();
+    final TreeItem treeItem = _viewMap.remove( entity );
+    if ( null != treeItem )
+    {
+      treeItem.remove();
+    }
   }
 
   @Override
   public void attributeChanged( final EntityChangeEvent event )
   {
     LOG.info( "attributeChanged(" + event + ")" );
+    final Object entity = event.getObject();
+    if ( entity instanceof Building )
+    {
+      final Building building = (Building) entity;
+      final String name = building.getName();
+      final TreeItem treeItem = _viewMap.get( building );
+      if ( null != treeItem )
+      {
+        treeItem.setText( name );
+      }
+    }
+    else if ( entity instanceof Room )
+    {
+      final Room room = (Room) entity;
+      final TreeItem treeItem = _viewMap.get( room );
+      if ( null != treeItem )
+      {
+        treeItem.setWidget( createRoomWidget( room ) );
+      }
+    }
   }
 
   @Override
   public void relatedAdded( final EntityChangeEvent event )
   {
     LOG.info( "relatedAdded(" + event + ")" );
+    final Object value = event.getValue();
+    final Object object = event.getObject();
+    if ( object instanceof Building && value instanceof Room )
+    {
+      final Room room = (Room) value;
+      final Building building = (Building) object;
+      final TreeItem parent = _viewMap.get( building );
+      if ( null != parent )
+      {
+        addRoom( parent, room );
+      }
+    }
+  }
+
+  private void addRoom( final TreeItem parent, final Room room )
+  {
+    final TreeItem treeItem = parent.addItem( createRoomWidget( room ) );
+    treeItem.setUserObject( room );
+    _viewMap.put( room, treeItem );
+  }
+
+  private HorizontalPanel createRoomWidget( final Room room )
+  {
+    final HorizontalPanel panel = new HorizontalPanel();
+    panel.add( new Label( room.getName() ) );
+    final Button delete = new Button( "X" );
+    delete.addClickHandler( new ClickHandler()
+    {
+      @Override
+      public void onClick( final ClickEvent event )
+      {
+        doDeleteRoom( room );
+      }
+    } );
+    panel.add( delete );
+    return panel;
+  }
+
+  private void doDeleteRoom( final Room room )
+  {
+    _buildingService.removeRoom( room.getID() );
   }
 
   @Override
   public void relatedRemoved( final EntityChangeEvent event )
   {
     LOG.info( "relatedRemoved(" + event + ")" );
+    final Object value = event.getValue();
+    final Object object = event.getObject();
+    if ( object instanceof Building && value instanceof Room )
+    {
+      final Room room = (Room) value;
+      final TreeItem treeItem = _viewMap.remove( room );
+      if ( null != treeItem )
+      {
+        treeItem.remove();
+      }
+    }
   }
 }
