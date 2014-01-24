9e9d765dc57ea1781f1d86210ac0c152c47619fe
Peter Donald <peter@realityforge.org>
2014-01-24 13:55:18 +1100

    Remove extra syntax

--- src/main/java/org/realityforge/replicant/example/server/service/AbstractExternalService.java
+++ src/main/java/org/realityforge/replicant/example/server/service/AbstractExternalService.java
@@ -8,7 +8,7 @@ import org.realityforge.replicant.server.ee.Replicate;
  * Abstract base class for services exposed to external agents.
  */
 @Replicate
-@Interceptors( { TyrellReplicationInterceptor.class } )
+@Interceptors(TyrellReplicationInterceptor.class)
 public abstract class AbstractExternalService
 {
 }
