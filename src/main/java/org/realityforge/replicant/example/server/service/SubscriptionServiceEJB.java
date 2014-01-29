07bdb8f26fe825c5d7d0a527687bf1bd3faf7527
Peter Donald <peter@realityforge.org>
2014-01-29 19:33:18 +1100

    Rework generated session so that it extends SimpleSessionInfo

--- src/main/java/org/realityforge/replicant/example/server/service/SubscriptionServiceEJB.java
+++ src/main/java/org/realityforge/replicant/example/server/service/SubscriptionServiceEJB.java
@@ -1,42 +1,28 @@
 package org.realityforge.replicant.example.server.service;
 
-import java.io.Serializable;
-import java.util.Collection;
 import java.util.LinkedList;
-import java.util.Map;
-import java.util.UUID;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.ejb.Local;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
 import org.realityforge.replicant.example.server.entity.Roster;
-import org.realityforge.replicant.example.server.entity.TyrellGraphEncoder;
-import org.realityforge.replicant.example.server.entity.TyrellRouterImpl;
+import org.realityforge.replicant.example.server.entity.TyrellSession;
 import org.realityforge.replicant.example.server.entity.dao.RosterRepository;
 import org.realityforge.replicant.example.server.entity.dao.RosterTypeRepository;
 import org.realityforge.replicant.server.EntityMessage;
 import org.realityforge.replicant.server.EntityMessageEndpoint;
 import org.realityforge.replicant.server.json.JsonEncoder;
-import org.realityforge.replicant.server.transport.EntityMessageAccumulator;
 import org.realityforge.replicant.server.transport.Packet;
 import org.realityforge.replicant.server.transport.PacketQueue;
-import org.realityforge.ssf.InMemorySessionManager;
 import org.realityforge.ssf.SessionManager;
 
 @Singleton
 @Local({ EntityMessageEndpoint.class, SubscriptionService.class, SessionManager.class })
 public class SubscriptionServiceEJB
-  extends InMemorySessionManager<TyrellSessionInfo>
-  implements SubscriptionService, EntityMessageEndpoint
+  extends AbstractTyrellSessionManager
+  implements SubscriptionService
 {
-  private static final Logger LOG = Logger.getLogger( SubscriptionServiceEJB.class.getName() );
-
-  @Inject
-  private TyrellGraphEncoder _encoder;
-
   @Inject
   private RosterTypeRepository _rosterTypeRepository;
 
@@ -49,8 +35,8 @@ public class SubscriptionServiceEJB
   public String poll( @Nonnull final String clientID, final int lastSequenceAcked )
     throws BadSessionException
   {
-    final TyrellSessionInfo session = ensureSession( clientID );
-    final PacketQueue queue = session.getSession().getQueue();
+    final TyrellSession session = ensureSession( clientID );
+    final PacketQueue queue = session.getQueue();
     final Packet packet;
     synchronized ( session )
     {
@@ -71,32 +57,32 @@ public class SubscriptionServiceEJB
   public void downloadAll( @Nonnull final String clientID )
     throws BadSessionException
   {
-    final TyrellSessionInfo session = ensureSession( clientID );
-    if ( !session.getSession().isInterestedInRosterList() )
+    final TyrellSession session = ensureSession( clientID );
+    if ( !session.isInterestedInRosterList() )
     {
       downloadAll( session );
     }
   }
 
-  private void downloadAll( @Nonnull final TyrellSessionInfo session )
+  private void downloadAll( @Nonnull final TyrellSession session )
   {
     final LinkedList<EntityMessage> messages = new LinkedList<>();
     for ( final Roster roster : _rosterRepository.findAll() )
     {
-      session.getSession().registerInterestInRoster( roster.getID() );
-      _encoder.encodeRoster( messages, roster );
+      session.registerInterestInRoster( roster.getID() );
+      getEncoder().encodeRoster( messages, roster );
     }
-    session.getSession().getQueue().addPacket( messages );
+    session.getQueue().addPacket( messages );
   }
 
   @Override
   public void subscribeToAll( @Nonnull final String clientID )
     throws BadSessionException
   {
-    final TyrellSessionInfo session = ensureSession( clientID );
-    if ( !session.getSession().isInterestedInRosterList() )
+    final TyrellSession session = ensureSession( clientID );
+    if ( !session.isInterestedInRosterList() )
     {
-      session.getSession().setInterestedInRosterList( true );
+      session.setInterestedInRosterList( true );
       downloadAll( session );
     }
   }
@@ -105,13 +91,13 @@ public class SubscriptionServiceEJB
   public void subscribeToMetaData( @Nonnull final String clientID )
     throws BadSessionException
   {
-    final TyrellSessionInfo session = ensureSession( clientID );
-    if ( !session.getSession().isInterestedInMetaData() )
+    final TyrellSession session = ensureSession( clientID );
+    if ( !session.isInterestedInMetaData() )
     {
-      session.getSession().setInterestedInMetaData( true );
+      session.setInterestedInMetaData( true );
       final LinkedList<EntityMessage> messages = new LinkedList<>();
-      _encoder.encodeObjects( messages, _rosterTypeRepository.findAll() );
-      session.getSession().getQueue().addPacket( messages );
+      getEncoder().encodeObjects( messages, _rosterTypeRepository.findAll() );
+      session.getQueue().addPacket( messages );
     }
   }
 
@@ -119,20 +105,20 @@ public class SubscriptionServiceEJB
   public void unsubscribeFromMetaData( @Nonnull final String clientID )
     throws BadSessionException
   {
-    ensureSession( clientID ).getSession().setInterestedInMetaData( false );
+    ensureSession( clientID ).setInterestedInMetaData( false );
   }
 
   @Override
   public void subscribeToRoster( @Nonnull final String clientID, @Nonnull final Roster roster )
     throws BadSessionException
   {
-    final TyrellSessionInfo session = ensureSession( clientID );
-    if ( !session.getSession().isRosterInteresting( roster.getID() ) )
+    final TyrellSession session = ensureSession( clientID );
+    if ( !session.isRosterInteresting( roster.getID() ) )
     {
-      session.getSession().registerInterestInRoster( roster.getID() );
+      session.registerInterestInRoster( roster.getID() );
       final LinkedList<EntityMessage> messages = new LinkedList<>();
-      _encoder.encodeRoster( messages, roster );
-      session.getSession().getQueue().addPacket( messages );
+      getEncoder().encodeRoster( messages, roster );
+      session.getQueue().addPacket( messages );
     }
   }
 
@@ -140,20 +126,20 @@ public class SubscriptionServiceEJB
   public void unsubscribeFromRoster( @Nonnull final String clientID, @Nonnull final Roster roster )
     throws BadSessionException
   {
-    ensureSession( clientID ).getSession().deregisterInterestInRoster( roster.getID() );
+    ensureSession( clientID ).deregisterInterestInRoster( roster.getID() );
   }
 
   @Override
   public void subscribeToRosterList( @Nonnull final String clientID )
     throws BadSessionException
   {
-    final TyrellSessionInfo session = ensureSession( clientID );
-    if ( !session.getSession().isInterestedInRosterList() )
+    final TyrellSession session = ensureSession( clientID );
+    if ( !session.isInterestedInRosterList() )
     {
-      session.getSession().setInterestedInRosterList( true );
+      session.setInterestedInRosterList( true );
       final LinkedList<EntityMessage> messages = new LinkedList<>();
-      _encoder.encodeObjects( messages, _rosterRepository.findAll() );
-      session.getSession().getQueue().addPacket( messages );
+      getEncoder().encodeObjects( messages, _rosterRepository.findAll() );
+      session.getQueue().addPacket( messages );
     }
   }
 
@@ -161,80 +147,6 @@ public class SubscriptionServiceEJB
   public void unsubscribeFromRosterList( @Nonnull final String clientID )
     throws BadSessionException
   {
-    ensureSession( clientID ).getSession().setInterestedInRosterList( false );
-  }
-
-  @SuppressWarnings("SynchronizationOnLocalVariableOrMethodParameter")
-  @Override
-  public void saveEntityMessages( @Nonnull final Collection<EntityMessage> messages )
-  {
-    if ( LOG.isLoggable( Level.FINER ) )
-    {
-      LOG.fine( "EntityMessage messages generated during service execution: " + messages );
-    }
-
-    //TODO: Rewrite this so that we add clients to indexes rather than searching through everyone for each change!
-    final Map<String, TyrellSessionInfo> sessions = getSessions();
-    final EntityMessageAccumulator accumulator = new EntityMessageAccumulator();
-    synchronized ( sessions )
-    {
-      for ( final EntityMessage message : messages )
-      {
-        final Map<String, Serializable> routingKeys = message.getRoutingKeys();
-
-        final Integer rosterID = (Integer) routingKeys.get( TyrellRouterImpl.ROSTER_KEY );
-        if ( null != rosterID )
-        {
-          for ( final TyrellSessionInfo sessionInfo : sessions.values() )
-          {
-            if ( sessionInfo.getSession().isRosterInteresting( rosterID ) )
-            {
-              accumulator.addEntityMessage( sessionInfo.getSession().getQueue(), message );
-            }
-          }
-        }
-        if ( null != routingKeys.get( TyrellRouterImpl.META_DATA_KEY ) )
-        {
-          for ( final TyrellSessionInfo sessionInfo : sessions.values() )
-          {
-            if ( sessionInfo.getSession().isInterestedInMetaData() )
-            {
-              accumulator.addEntityMessage( sessionInfo.getSession().getQueue(), message );
-            }
-          }
-        }
-        if ( null != routingKeys.get( TyrellRouterImpl.ROSTER_LIST_KEY ) )
-        {
-          for ( final TyrellSessionInfo sessionInfo : sessions.values() )
-          {
-            if ( sessionInfo.getSession().isInterestedInRosterList() )
-            {
-              accumulator.addEntityMessage( sessionInfo.getSession().getQueue(), message );
-            }
-          }
-        }
-      }
-    }
-
-    accumulator.complete();
-  }
-
-  @Nonnull
-  @Override
-  protected TyrellSessionInfo newSessionInfo()
-  {
-    return new TyrellSessionInfo( UUID.randomUUID().toString() );
-  }
-
-  @Nonnull
-  private TyrellSessionInfo ensureSession( @Nonnull final String clientID )
-    throws BadSessionException
-  {
-    final TyrellSessionInfo session = getSession( clientID );
-    if ( null == session )
-    {
-      throw new BadSessionException();
-    }
-    return session;
+    ensureSession( clientID ).setInterestedInRosterList( false );
   }
 }
