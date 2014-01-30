112501674b16f417df90807d5eb5bbf17edbbdd4
Peter Donald <peter@realityforge.org>
2014-01-31 10:11:31 +1100

    Start to incorporate a RequestManager

--- src/main/java/org/realityforge/replicant/client/transport/ClientSession.java
+++ src/main/java/org/realityforge/replicant/client/transport/ClientSession.java
@@ -9,14 +9,28 @@ import javax.annotation.Nonnull;
 public abstract class ClientSession
 {
   private final String _sessionID;
+  private final RequestManager _requestManager;
 
   public ClientSession( @Nonnull final String sessionID )
   {
     _sessionID = sessionID;
+    _requestManager = newRequestManager();
   }
 
+  @Nonnull
   public String getSessionID()
   {
     return _sessionID;
+  }
+
+  @Nonnull
+  public RequestManager getRequestManager()
+  {
+    return _requestManager;
+  }
+
+  protected RequestManager newRequestManager()
+  {
+    return new RequestManager();
   }
 }
