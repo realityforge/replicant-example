5bf7306a0cd8476c28e425942ad1ce29c1f9447b
Peter Donald <peter@realityforge.org>
2014-03-17 20:34:41 +1100

    Remove completed TODO

--- src/main/java/org/realityforge/replicant/example/client/services/TyrellDataLoaderService.java
+++ src/main/java/org/realityforge/replicant/example/client/services/TyrellDataLoaderService.java
@@ -33,9 +33,6 @@ import org.realityforge.replicant.example.client.service.GwtRpcSubscriptionServi
 import org.realityforge.replicant.example.client.service.TyrellGwtRpcAsyncCallback;
 import org.realityforge.replicant.shared.transport.ReplicantContext;
 
-/**
- * TODO: Rework the generated to accept arbitrary parameters subscriptionManager
- */
 public class TyrellDataLoaderService
   extends GwtDataLoaderService<TyrellClientSessionImpl, TyrellReplicationGraph>
   implements DataLoaderService
