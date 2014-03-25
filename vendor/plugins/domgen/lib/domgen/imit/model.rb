a2695d04be34cedb27d0b745144aeda0cb6f5c6c
Peter Donald <peter@realityforge.org>
2014-03-26 09:29:33 +1100

    Remove unused transport_id

--- vendor/plugins/domgen/lib/domgen/imit/model.rb
+++ vendor/plugins/domgen/lib/domgen/imit/model.rb
@@ -260,8 +260,6 @@ module Domgen
         super(application, options, &block)
       end
 
-      attr_accessor :transport_id
-
       attr_reader :application
 
       attr_reader :name
@@ -573,11 +571,6 @@ module Domgen
       end
 
       def post_verify
-        index = 0
-        repository.imit.graphs.each do |graph|
-          graph.transport_id = index
-          index += 1
-        end
         index = 0
         repository.data_modules.select { |data_module| data_module.imit? }.each do |data_module|
           data_module.entities.each do |entity|
