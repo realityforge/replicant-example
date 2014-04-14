904ac10183c565ba166920f9111ca44f8283b04e
Peter Donald <peter@realityforge.org>
2014-04-15 08:05:53 +1000

    Make sure domain is created and setup

--- config/setup.sh
+++ config/setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+R=`(asadmin list-domains | grep -q 'tyrell ') && echo yes`
+if [ "$R" != 'yes' ]; then
+  asadmin create-domain tyrell
+fi
+
+R=`(asadmin list-domains | grep -q 'tyrell running') && echo yes`
+if [ "$R" != 'yes' ]; then
+  asadmin start-domain tyrell
+fi
+
 R=`(asadmin list-libraries | grep -q postgresql-9.1-901.jdbc4.jar) && echo yes`
 if [ "$R" != 'yes' ]; then
   asadmin add-library ~/.m2/repository/postgresql/postgresql/9.1-901.jdbc4/postgresql-9.1-901.jdbc4.jar
+  asadmin restart-domain tyrell
 fi
 
 asadmin delete-jdbc-resource jdbc/Tyrell
