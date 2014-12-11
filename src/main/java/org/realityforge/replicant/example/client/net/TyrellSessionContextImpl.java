1c921ac784e6300e38b59eb32e29da2c675d5542
Peter Donald <peter@realityforge.org>
2014-12-12 09:52:14 +1100

    Make the client-side routing match the server-side routing approach

--- src/main/java/org/realityforge/replicant/example/client/net/TyrellSessionContextImpl.java
+++ src/main/java/org/realityforge/replicant/example/client/net/TyrellSessionContextImpl.java
@@ -1,32 +1,23 @@
 package org.realityforge.replicant.example.client.net;
 
-import java.io.Serializable;
 import java.util.Date;
-import java.util.Map;
 import javax.annotation.Nonnull;
-import javax.inject.Inject;
+import javax.annotation.Nullable;
 import org.realityforge.gwt.datatypes.client.date.RDate;
 import org.realityforge.replicant.example.client.data_type.RosterSubscriptionDTO;
 
 public class TyrellSessionContextImpl
   implements TyrellSessionContext
 {
-  private final TyrellClientRouter _router;
-
-  @Inject
-  public TyrellSessionContextImpl( final TyrellClientRouter router )
-  {
-    _router = router;
-  }
-
   @Override
-  public boolean doesShiftListMatchEntity( @Nonnull final RosterSubscriptionDTO filter, @Nonnull final Object entity )
+  public boolean doesShiftListMatchEntity( @Nonnull final RosterSubscriptionDTO filter,
+                                           @Nonnull final Object entity,
+                                           @Nonnull final Integer rosterID,
+                                           @Nullable final Date shiftStartAt )
   {
     final RDate startAt = filter.getStartOn();
     final Date startOn = RDate.toDate( startAt );
     final Date endDate = RDate.toDate( startAt.addDays( filter.getNumberOfDays() ) );
-    final Map<String, Serializable> route = _router.route( entity );
-    final Date shiftStartAt = (Date) route.get( TyrellClientRouter.SHIFT_LIST_TYRELL_SHIFT_START_AT_KEY );
     return null == shiftStartAt ||
            ( ( startOn.before( shiftStartAt ) || startOn.equals( shiftStartAt ) ) &&
              endDate.after( shiftStartAt ) );
