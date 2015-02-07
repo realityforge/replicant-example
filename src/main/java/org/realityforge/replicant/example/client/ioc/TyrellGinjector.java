893188ee7e0948dd445b327702fc6395a6aa8b94
Peter Donald <peter@realityforge.org>
2015-02-07 12:48:20 +1100

    Add base replicant gin module

--- src/main/java/org/realityforge/replicant/example/client/ioc/TyrellGinjector.java
+++ src/main/java/org/realityforge/replicant/example/client/ioc/TyrellGinjector.java
@@ -2,9 +2,10 @@ package org.realityforge.replicant.example.client.ioc;
 
 import com.google.gwt.inject.client.GinModules;
 import com.google.gwt.inject.client.Ginjector;
+import org.realityforge.replicant.client.json.gwt.ReplicantGinModule;
 import org.realityforge.replicant.example.client.ui.SimpleUI;
 
-@GinModules( { BasicModule.class, TyrellImitServicesModule.class, TyrellGwtRpcServicesModule.class } )
+@GinModules( { BasicModule.class, ReplicantGinModule.class, TyrellImitServicesModule.class, TyrellGwtRpcServicesModule.class } )
 public interface TyrellGinjector
   extends Ginjector
 {
